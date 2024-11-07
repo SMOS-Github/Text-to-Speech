99c62e075db33e0205348cb0de5f6be5065ebd4e
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-07 23:25:40 +0530

    Update
    Cleanup.

--- app.js
+++ app.js
@@ -1,50 +1,38 @@
-const message = new SpeechSynthesisUtterance();
-//console.log(message);
-let voices = [];
-const voicesDopdown = document.querySelector('[name="voice"]');//this style tells that element can access the inside element without calling like array of foreach
-//console.log(voicesDopdown);
 const options = document.querySelectorAll('[type="range"],[name="text"]');
-//console.log(options);
-
+const voicesDopdown = document.querySelector('[name="voice"]');
 const playBtn = document.querySelector(".play");
 const stopBtn = document.querySelector(".stop");
-message.text = document.querySelector('[name="text"]').value;
+const message = new SpeechSynthesisUtterance();
 
-//console.log(message.text);
+let voices = [];
+message.text = document.querySelector('[name="text"]').value;
 
 function ShowVoiceModules() {
     voices = this.getVoices();
-    //console.log(voices); //show all voices data availabe according to os and browser in Edge its around 400 voice modules
     const voiceOptions = voices
         .map(voice => `<option value="${voice.name}">${voice.name}(${voice.lang})</opton>`)
         .join('');
     voicesDopdown.innerHTML = voiceOptions;
 
-    console.log(voiceOptions);
 }
 
 function SetVoice() {
-    console.log(this.value);
     message.voice = voices.find(voice => voice.name === this.value);
     Toggle();
 }
-function Toggle(speaking=true) {
+function Toggle(speaking = true) {
     speechSynthesis.cancel();
     if (speaking) {
         speechSynthesis.speak(message);
     }
 }
 function SetOption() {
-    console.log(this.name, this.value);
+
     message[this.name] = this.value;
     Toggle();
 }
 speechSynthesis.addEventListener('voiceschanged', ShowVoiceModules);
 voicesDopdown.addEventListener('change', SetVoice);
-
-options.forEach(option =>
-    option.addEventListener('change', SetOption)
-);
-
+options.forEach(option => option.addEventListener('change', SetOption));
 playBtn.addEventListener('click', Toggle);
 stopBtn.addEventListener('click', () => Toggle(false));
